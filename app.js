b73b55a3ad402c40b3a24f532c393305ef8aa020
Netrunner <kk2592121@gmail.com>
2022-08-24 11:19:56 +0800

    update

--- app.js
+++ app.js
@@ -39,7 +39,6 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
