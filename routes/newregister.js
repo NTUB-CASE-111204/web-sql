de05dd1977dbcfb47ba96f576e011300f29a76f7
11036017 <10556026@ntub.edu.tw>
2022-10-23 15:55:18 +0800

    會員頭像上傳

--- routes/newregister.js
+++ routes/newregister.js
@@ -4,18 +4,55 @@ var router = express.Router();
 //增加引用函式
 const member = require('./utility/member');
 
+//---------------------------
+// 引用multer外掛
+//---------------------------
+const multer  = require('multer');
+
+// 宣告上傳存放空間及檔名更改
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/pic');
+    },
+
+    filename: function (req, file, cb) {
+        cb(null, Date.now()+"--"+file.originalname);    
+    }   
+})
+
+// 產生multer的上傳物件
+var maxSize=1024*1024;  //設定最大可接受圖片大小(1M)
+
+var upload = multer({
+    storage:storage
+})
+//---------------------------
+
 //接收POST請求
-router.post('/', function (req, res, next) {
+router.post('/', upload.single('m_pic'), function (req, res, next) {
+  // 如果有選擇圖片
+  if (typeof req.file != 'undefined'){
+    // 傳入檔案不可超過maxSize
+    if(req.file.size > maxSize){
+        res.render('fileSizeError');  //圖片過大
+        return;
+    }                      
+  } 
+
   var m_email = req.body.m_email;
   var m_password = req.body.m_password;
   var m_nickname = req.body.m_nickname;
   var m_sex = req.body.m_sex;
-  var m_pic = req.body.m_pic;
+  var m_pic;
   var m_birth = req.body.m_birth;
   var m_phone = req.body.m_phone;
   //var m_phone = Number(req.body.m_phone);
-  
-  console.log("1/" + m_birth);
+
+  // 如果有選擇圖片
+  if (typeof(req.file) != 'undefined'){
+    m_pic=req.file.filename;   //取得上傳照片名稱
+  }
+
   // 建立一個新資料物件
   var newData = {
     m_email:m_email,
@@ -27,7 +64,7 @@ router.post('/', function (req, res, next) {
     m_phone:m_phone
   }
 
-  console.log("2/" + m_email);
+  console.log("2/" + m_pic);
   member.add(newData).then(d => {
     console.log("3/" + d);
     if (d == 0) {
