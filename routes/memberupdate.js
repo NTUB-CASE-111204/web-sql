fa5384b760bfeb97d9a984108e087e7b3c36284b
11036020 <11036020@ntub.edu.tw>
2022-11-19 14:48:17 +0800

    預覽頭貼

--- routes/memberupdate.js
+++ routes/memberupdate.js
@@ -4,9 +4,46 @@ var router = express.Router();
 //增加引用函式
 const member = require('./utility/member');
 
+//---------------------------
+// 引用multer外掛
+//---------------------------
+const multer  = require('multer');
+
+// 宣告上傳存放空間及檔名更改
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/pic');
+    },
+
+    filename: function (req, file, cb) {
+        cb(null, Date.now()+"--"+file.originalname);    
+    }   
+})
+
+// 產生multer的上傳物件
+var maxSize=1024*1024;  //設定最大可接受圖片大小(1M)
+
+var upload = multer({
+    storage:storage
+})
+//---------------------------
+
 //接收POST請求
-router.post('/', function (req, res, next) {
+router.post('/', upload.single('m_pic'), function (req, res, next) {
   var m_email = req.body.m_email;   //取得會員帳號
+  // 如果有選擇圖片
+  if (typeof req.file != 'undefined'){
+  // 傳入檔案不可超過maxSize
+  if(req.file.size > maxSize){
+      res.render('fileSizeError');  //圖片過大
+      return;
+    }                      
+  } 
+
+  // 如果有選擇圖片
+  if (typeof(req.file) != 'undefined'){
+      m_pic=req.file.filename;   //取得上傳照片名稱
+  }
 
   var newData = {
     m_email: m_email,  
