73fb97d1b60b00e29cd3d8bdfafdb6cb52e3d0b5
11036017 <10556026@ntub.edu.tw>
2022-11-20 21:55:22 +0800

    會員介面(動物清單)

--- routes/donate.js
+++ routes/donate.js
@@ -1,22 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('donate', { title: 'Express' });
+//增加引用函式
+const animal = require('./utility/animal');
+
+//接收GET請求
+router.get('/', function (req, res, next) {
+  animal.list().then(data => {
+    if (data == null) {
+      res.render('error');  //導向錯誤頁面
+    } else if (data.length > 0) {
+      for (i = 0; i < data.length; i++) {
+        if (data[i].an_postdate != null) {
+          data[i].an_postdate = data[i].an_postdate.getFullYear() + "-" + (data[i].an_postdate.getMonth() + 1) + "-" + data[i].an_postdate.getDate();
+        }
+      }
+      for (i = 0; i < data.length; i++) {
+        if (data[i].an_pic != null) {
+          data[i].an_pic = "animalpic/" + data[i].an_pic;
+        }
+      }
+      res.render('donate', { items: data });  //將資料傳給顯示頁面
+    } else {
+      res.render('notFound');  //導向找不到頁面
+    }
+  })
 });
 
 module.exports = router;
-
-// var server = require("./server"); 
-// var router = require("./router"); 
-// var requestHandlers = require("./requestHandlers"); 
- 
-// var handle = {} 
-// //區分大小寫的 
-// handle["/"] = requestHandlers.start; 
-// handle["/start"] = requestHandlers.start; 
-// handle["/upload"] = requestHandlers.upload; 
-// handle["/show"] = requestHandlers.show; 
- 
-// server.start(router.route, handle); 
