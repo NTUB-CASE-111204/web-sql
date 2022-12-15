192c57d17c84fe3e4a90f95d3c291ba2a26b6056
11036013 <11036013@ntub.edu.tw>
2022-12-15 16:55:34 +0800

    測試完改回原本的程式

--- routes/memberlogin.js
+++ routes/memberlogin.js
@@ -13,49 +13,41 @@ const formatDate = (current_datetime)=>{
 router.post('/', function(req, res, next) {
     var m_email = req.body.m_email;                 //取得帳號
     var m_password = req.body.m_password;     //取得密碼
-    var callback = req.body.callback;                 //取得帳號
-    console.log(callback);
-    if (callback==""){
-        res.render('loginFail');  //傳至登入失敗
-        }
-        else{
-        //captcha
-        member.login(m_email, m_password).then(d => {
-            if (d==null){
-                req.session.m_email = null;
-                req.session.m_password = null;           
-                res.render('loginFail');  //傳至登入失敗
-            }else if(d.admin==true){
-                req.session.m_email = d.m_email;
-                req.session.m_password = d.m_password;
-                if(d.m_birth == null || d.m_birth == ''){
-                    d.m_birth = "尚未填寫"
-                }else{
-                    d.m_birth = formatDate(d.m_birth)
-                }
-                if(d.m_phone == null || d.m_phone == ''){
-                    d.m_phone = "尚未填寫"
-                }
-                res.render('empindex', {item:d});     //導向使用者
+
+    member.login(m_email, m_password).then(d => {
+        if (d==null){
+            req.session.m_email = null;
+            req.session.m_password = null;           
+            res.render('loginFail');  //傳至登入失敗
+        }else if(d.admin==true){
+            req.session.m_email = d.m_email;
+            req.session.m_password = d.m_password;
+            if(d.m_birth == null || d.m_birth == ''){
+                d.m_birth = "尚未填寫"
             }else{
-                req.session.m_email = d.m_email;
-                req.session.m_password = d.m_password;
-                if (d.m_pic == null || d.m_pic == ''){
-                    d.m_pic = "icon-07.png";
-                }
-                if(d.m_birth == null || d.m_birth == ''){
-                    d.m_birth = "尚未填寫"
-                }else{
-                    d.m_birth = formatDate(d.m_birth)
-                }
-                if(d.m_phone == null || d.m_phone == ''){
-                    d.m_phone = "尚未填寫"
-                }
-                res.render('memberpage', {item:d});     //導向使用者
-                callback == ""
-            }  
-        })
-    
+                d.m_birth = formatDate(d.m_birth)
+            }
+            if(d.m_phone == null || d.m_phone == ''){
+                d.m_phone = "尚未填寫"
+            }
+            res.render('empindex', {item:d});     //導向使用者
+        }else{
+            req.session.m_email = d.m_email;
+            req.session.m_password = d.m_password;
+            if (d.m_pic == null || d.m_pic == ''){
+                d.m_pic = "icon-07.png";
+            }
+            if(d.m_birth == null || d.m_birth == ''){
+                d.m_birth = "尚未填寫"
+            }else{
+                d.m_birth = formatDate(d.m_birth)
+            }
+            if(d.m_phone == null || d.m_phone == ''){
+                d.m_phone = "尚未填寫"
+            }
+            res.render('memberpage', {item:d});     //導向使用者
+        }  
+    })
 });
 
 module.exports = router;
